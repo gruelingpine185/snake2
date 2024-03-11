6bb03cb04dc542572088a60077bf7d3928173954
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-03-11 18:39:13 -0400

    game class methods

--- inc/game.h
+++ inc/game.h
@@ -18,6 +18,10 @@ namespace s2 {
     public:
         game(const size<float>& _cell_size) noexcept;
         ~game() noexcept;
+
+        void handle_events(const SDL_Event& _event);
+        void update(const float _dt) noexcept;
+        bool render(SDL_Renderer* _renderer) noexcept;
     private:
         std::random_device _rnd_device;
         std::mt19937 _prng;        
--- src/game.cpp
+++ src/game.cpp
@@ -12,4 +12,18 @@ namespace s2 {
         {}
 
     game::~game() noexcept {}
+
+    void game::handle_events(const SDL_Event& _event) {
+        this->_snake.handle_events(_event);
+    }
+
+    void game::update(const float _dt) noexcept {
+        this->_snake.update(_dt);
+    }
+
+    bool game::render(SDL_Renderer* _renderer) noexcept {
+        if(!this->_snake.render(_renderer)) return false;
+
+        return true;
+    }
 }
