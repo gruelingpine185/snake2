32389674a1c6c4f3dbb02915247ffefa451e45cf
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-03-11 18:40:41 -0400

    use game class (tick is temporary)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <string>
 
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_events.h>
 
 #include "window.h"
+#include "game.h"
+#include "pos.inl"
+#include "size.inl"
 
 
 int main() {
@@ -14,14 +18,23 @@ int main() {
     if(!win.create()) return 1;
     if(!win.show()) return 1;
 
+    s2::game game(cell);
+    float tick = 0.0f;
+
     while(!win.should_close()) {
         SDL_Event event;
 
         while(SDL_PollEvent(&event)) {
             win.handle_events(event);
+            game.handle_events(event);
         }
 
-        win.render();
+        game.update(tick);
+        if(!win.clear()) return 1;
+        if(!game.render(win.renderer())) return 1;
+        if(!win.render()) return 1;
+
+        tick++;
     }
 
     return 0;
